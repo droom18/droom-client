c10d25bd0f841545939a60213589c8dd77809a9b
ykrueng <yasirah.krueng@gmail.com>
2019-05-06 11:24:53 -0500

    initial commit - create react app

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Droom</h1>
+        <p>Test for GitHub PR</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
