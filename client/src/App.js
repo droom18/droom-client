76be181cc123cd042f27473538141c3532506680
Sean Doyle <sdoylelambda@gmail.com>
2019-05-07 19:21:45 -0400

    Minor changes.

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
 import Login from "./components/Login/Login";
-import Navbar from "./components/Navbar/Navbar";
-import { Route } from 'react-router-dom';
+// import Navbar from "./components/Navbar/Navbar";
+// import { Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Navbar />
+        {/* <Navbar /> */}
         <div>
-          <Route exact path="/" component={Home} />
+          {/* <Route exact path="/" component={Home} />
           <Route path="/about" component={SchoolList} />
-          <Route path="/contact" component={Contact} />
+          <Route path="/contact" component={Contact} /> */}
         </div>
         <Login />
         <p>Test for GitHub PR</p>
