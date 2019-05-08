6b63ee6bfe156c82b20596f2e632a2e2cbe2a193
Sean Doyle <sdoylelambda@gmail.com>
2019-05-08 16:44:42 -0400

    NewSchool for adding a new school.

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import NewUser from "../src/components/NewUser/NewUser";
 import Homepage from "../src/components/Homepage/Homepage";
 import axios from 'axios';
 import Admin from './components/Admin/Admin';
+import NewSchool from './components/NewSchool/NewSchool'
 
 // change to class -- get state -- is loggedIn? bool -- and have token(string)
 class App extends React.Component {
@@ -40,6 +41,7 @@ class App extends React.Component {
             <Route exact path="/schools/schoolRoutes" render={props => <Schools {...props} isLoggedIn = {this.state} />}/>
             <Route path="/credentials/registerRoutes" component={NewUser} />
             <Route path="/admins/adminRoutes" render={props => <Admin {...props} isLoggedIn = {this.state} />}/>
+            <Route path="/donors/donorRoutes" component={NewSchool} />
           </div>
 
           <p>Test for GitHub PR</p>
