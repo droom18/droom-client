4221ca7f936f93c6e9f01601a9e670914ec05a0b
Leslie Thompson <lcrthompson@gmail.com>
2019-05-08 08:40:14 -0700

    link to create profile on homepage

--- client/src/components/Homepage/Homepage.js
+++ client/src/components/Homepage/Homepage.js
@@ -1,12 +1,13 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 
 const Homepage = () => {
   return (
     <div>
-        <p>list of schools in need</p>
+      <p>list of schools in need</p>
+      <Link className="sign-up" to="/credentials/registerRoutes">
         <button>Create profile</button>
-        <button>Create profile</button>
+      </Link>
     </div>
   );
 };
