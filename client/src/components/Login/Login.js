b7fdc00cbc7c485367cea2bb30e3f970a861515b
Leslie Thompson <lcrthompson@gmail.com>
2019-05-08 14:57:10 -0700

    change

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -3,12 +3,12 @@ import LoginForm from "../LoginForm/index";
 import NewUser from "../NewUser/NewUser";
 import Schools from "../School/Schools";
 
-const Login = (props) => {
+const Login = props => {
   return (
     <div>
       <h1>Login Page</h1>
       <button>To Donate page link!</button>
-      <LoginForm login={this.props.login}/>
+      <LoginForm login={this.props.login} />
       {/* <NewUser /> */}
       {/* <Schools /> */}
     </div>
